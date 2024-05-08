8df141ce73912bbd98b006216343e02cca5cf4f4
hojoon.sung <hjsung94@gmail.com>
2024-05-08 17:38:03 +0900

    chore: 설정파일 반영

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    port: process.env.VUE_APP_SERVER_PORT,
+    https: false,
+    overlay: false,
+    compress: true,
+    disableHostCheck: true,
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+      },
+    },
+    devtool: 'cheap-source-map',
+  },
+  css: {
+    // extract: true,
+    extract: { ignoreOrder: true },
+    sourceMap: true,
+  },
+  runtimeCompiler: true,
+  productionSourceMap: false,
+};
